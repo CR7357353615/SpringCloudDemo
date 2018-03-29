67990dde6ef033313f107b6e608fba785b6a89f5
CR7357353615 <357353615@qq.com>
2018-03-29 17:01:52 +0800

    eidt

--- ConfigServer/src/main/java/com/feng/ConfigApplication.java
+++ ConfigServer/src/main/java/com/feng/ConfigApplication.java
@@ -5,12 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-/**
- * @author fengzp
- * @date 17/5/4
- * @email fengzp@gzyitop.com
- * @company 广州易站通计算机科技有限公司
- */
+
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 参考文章https://www.cnblogs.com/andyfengzp/p/6830679.html
+
 源代码:https://github.com/fengzp/SpringCloudDemo
-最后要用post的方式请求http://localhost:8030/bus/refresh处罚rabbitmq的刷新
+
+最后要用post的方式请求http://localhost:8030/bus/refresh触发rabbitmq的刷新
