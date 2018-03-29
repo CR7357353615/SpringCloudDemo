19d5cd9e1bb105a448b844d0c2cf10ba4011d911
CR7357353615 <357353615@qq.com>
2018-03-29 12:21:05 +0800

    first commit

--- EurekaServer/src/main/java/com/feng/EurekaServer.java
+++ EurekaServer/src/main/java/com/feng/EurekaServer.java
@@ -1,20 +1,14 @@
 package com.feng;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * @author fengzp
- * @date 17/4/27
- * @email fengzp@gzyitop.com
- * @company 广州易站通计算机科技有限公司
- */
 @SpringBootApplication
 @EnableEurekaServer
-public class ServerApplication {
-
+public class EurekaServer {
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+        SpringApplication.run(EurekaServer.class, args);
     }
 }
